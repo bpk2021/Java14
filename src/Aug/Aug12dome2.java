4691d6fd24a7d6ffc40af51aa84a0d02e53bbd70
bpk2021 <guoshuainan2021@163.com>
2021-08-12 17:17:01 +0800

    8.12dome

---
+++ src/Aug/Aug12dome2.java
@@ -0,0 +1,21 @@
+package Aug;
+
+/**
+ * 这是文档注释 /**回车
+ * @author Administrator
+ *
+ */
+public class Aug12dome2 {
+	
+	/**
+	 * 主函数
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello bpk");
+
+	}
+
+}
